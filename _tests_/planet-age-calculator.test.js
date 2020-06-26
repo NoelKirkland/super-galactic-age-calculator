3ad2623b5f3a0570429f22dc08bd1dc89dd07530
Noel Kirkland <noelkirkland@gmail.com>
2020-06-26 10:57:13 -0700

    successfully test my constructor function by defining all of my test objects values

--- _tests_/planet-age-calculator.test.js
+++ _tests_/planet-age-calculator.test.js
@@ -3,7 +3,7 @@ import { User } from '../src/planet-age-calculator.js';
 describe('User', () => {
   let reusableUser;
   beforeEach(() => {
-    reusableUser = new User(Noel, 27, 10, 20, 78, 14, 41);
+    reusableUser = new User("Noel", 27, "Male", 10, 20, 78, 14, 41);
   });
 
   test('Should create a new user object with a life expectancy, a Mars age and a Mars life expectancy', () => {
