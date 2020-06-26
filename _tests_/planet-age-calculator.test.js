5b562bdc4cc56dc3c6b626a391e1d50fb8168f5a
Noel Kirkland <noelkirkland@gmail.com>
2020-06-26 10:39:16 -0700

    test a reusable object that has all of the values that I will eventually add to my constructor function but have not yet add. Failed test because my values are not yet defined.

--- _tests_/planet-age-calculator.test.js
+++ _tests_/planet-age-calculator.test.js
@@ -3,10 +3,10 @@ import { User } from '../src/planet-age-calculator.js';
 describe('User', () => {
   let reusableUser;
   beforeEach(() => {
-    reusableUser = new User();
+    reusableUser = new User(Noel, 27, 10, 20, 78, 14, 41);
   });
 
-  test('Should create a new user object', () => {
+  test('Should create a new user object with a life expectancy, a Mars age and a Mars life expectancy', () => {
     console.log(reusableUser);
   });
 });
