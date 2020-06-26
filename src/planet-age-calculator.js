8a268e448fd394c094df2c87f31170e22e14e35b
Noel Kirkland <noelkirkland@gmail.com>
2020-06-26 10:46:56 -0700

    populate my user constructor function

--- src/planet-age-calculator.js
+++ src/planet-age-calculator.js
@@ -1,5 +1,14 @@
 // project business logic goes here
 // exports!
 export class User {
-  
+  constructor(firstName, age, gender, fireworkPropensity, nuclearProximity, lifeExpectancy, planetaryAge, planetaryLifeExpectancy){
+    this.firstName = firstName,
+    this.age = age,
+    this.gender = gender,
+    this.fireworkPropensity = fireworkPropensity,
+    this.nuclearProximity = nuclearProximity,
+    this.lifeExpectancy = lifeExpectancy,
+    this.planetaryAge = planetaryAge,
+    this.planetaryLifeExpectancy = planetaryLifeExpectancy
+  }
 }
