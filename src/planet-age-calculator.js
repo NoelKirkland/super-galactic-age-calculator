19e068fe748cddd46bb156c46b4606febba557c3
Noel Kirkland <noelkirkland@gmail.com>
2020-06-26 14:16:54 -0700

    create an empty venusCalculator for spec four

--- src/planet-age-calculator.js
+++ src/planet-age-calculator.js
@@ -47,5 +47,8 @@ export class User {
     this.planetaryAge = parseInt((this.age * 365/(365*0.24)).toFixed());
     this.planetaryLifeExpectancy = parseInt((this.lifeExpectancy * 365/(365*0.24)).toFixed());
   }
+  venusCalculator(){
+
+  }
 }
 
