12c3385470e22a7da3b6a9f6f5e5150cd64243ba
Noel Kirkland <noelkirkland@gmail.com>
2020-06-26 11:27:07 -0700

    successfully failed the first test of my second spec     Expected: 78     Received: null

--- _tests_/planet-age-calculator.test.js
+++ _tests_/planet-age-calculator.test.js
@@ -3,10 +3,14 @@ import { User } from '../src/planet-age-calculator.js';
 describe('User', () => {
   let reusableUser;
   beforeEach(() => {
-    reusableUser = new User("Noel", 27, "Male", 10, 20, 78, 14, 41);
+    reusableUser = new User("Noel", 27, "Male", 10, 20, null, 14, 41);
   });
 
   test('Should create a new user object with a life expectancy, a Mars age and a Mars life expectancy', () => {
     console.log(reusableUser);
+  });
+  test('Should correctly calculate life expectancy for user', () => {
+    reusableUser.lifeExpectancyCalculate();
+    expect(reusableUser.lifeExpectancy).toBe(78);
   });
 });
--- src/planet-age-calculator.js
+++ src/planet-age-calculator.js
@@ -10,7 +10,7 @@ export class User {
     this.planetaryLifeExpectancy = planetaryLifeExpectancy
   }
 
-  lifeExpectancy(){
+  lifeExpectancyCalculate(){
 
   }
 }
