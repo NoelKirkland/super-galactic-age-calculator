24e84f451ca72942f237274f39ef20654853b6a4
Noel Kirkland <noelkirkland@gmail.com>
2020-07-05 22:49:54 -0700

    pass first test on spec six. Meant to say spec six on last commit

--- src/planet-age-calculator.js
+++ src/planet-age-calculator.js
@@ -40,10 +40,11 @@ export class User {
     } else if (planet === "Venus"){
       this.planetaryAge = parseInt((this.age * 365/(365*0.62)).toFixed());
       this.planetaryLifeExpectancy = parseInt((this.lifeExpectancy * 365/(365*0.62)).toFixed());
-      this.planetaryLifeSpan = this.planetaryLifeExpectancy - this.planetaryAge
+      this.planetaryLifeSpan = this.planetaryLifeExpectancy - this.planetaryAge;
     } else if (planet === "Mars"){
       this.planetaryAge = parseInt((this.age * 365/(365*1.88)).toFixed());
       this.planetaryLifeExpectancy = parseInt((this.lifeExpectancy * 365/(365*1.88)).toFixed());
+      this.planetaryLifeSpan = this.planetaryLifeExpectancy - this.planetaryAge;
     } else if (planet === "Jupiter") {
       this.planetaryAge = parseInt((this.age * 365/(365*11.86)).toFixed());
       this.planetaryLifeExpectancy = parseInt((this.lifeExpectancy * 365/(365*11.86)).toFixed());
