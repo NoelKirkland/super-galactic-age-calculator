822c715c4a67145b30b0b02f5e199730660fbc97
Noel Kirkland <noelkirkland@gmail.com>
2020-07-05 23:24:29 -0700

    create new describe block but running into a weird bug off the bat. The only variable I changed is the gender from male to female. The Life expectancy should increase but for some reason it is decreasing

--- __tests__/planet-age-calculator.test.js
+++ __tests__/planet-age-calculator.test.js
@@ -3,7 +3,7 @@ import { User } from '../src/planet-age-calculator.js';
 describe('User', () => {
   let reusableUser;
   beforeEach(() => {
-    reusableUser = new User("Noel", 27, "Male", 10, "less than 1000 miles", null, null, null);
+    reusableUser = new User("Noel", 27, "Male", 10, "less than 1000 miles");
   });
 
   test('Should create a new user object with a life expectancy, a Mars age and a Mars life expectancy', () => {
@@ -11,6 +11,76 @@ describe('User', () => {
   test('Should correctly calculate life expectancy for user', () => {
     reusableUser.lifeExpectancyCalculator();
     expect(reusableUser.lifeExpectancy).toBe(68);
+  });
+  test('Should correctly calculate age on Mercury', () => {
+    reusableUser.planetCalculator("Mercury");
+    expect(reusableUser.planetaryAge).toBe(113);
+  });
+  test('Should correctly calculate life expectancy on Mercury', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Mercury");
+    expect(reusableUser.planetaryLifeExpectancy).toBe(283);
+  });
+  test('Should correctly calculate how much longer the user has left to live on Mercury', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Mercury");
+    expect(reusableUser.planetaryLifeSpan).toEqual(170);
+  });
+  test('Should correctly calculate age on Venus', () => {
+    reusableUser.planetCalculator("Venus");
+    expect(reusableUser.planetaryAge).toBe(44);
+  });
+  test('Should correctly calculate life expectancy on Venus', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Venus");
+    expect(reusableUser.planetaryLifeExpectancy).toBe(110);
+  });
+  test('Should correctly calculate how much longer the user has left to live on Venus', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Venus");
+    expect(reusableUser.planetaryLifeSpan).toEqual(66);
+  });
+  test('Should correctly calculate age on Mars', () => {
+    reusableUser.planetCalculator("Mars");
+    expect(reusableUser.planetaryAge).toBe(14);
+  });
+  test('Should correctly calculate life expectancy on Mars', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Mars");
+    expect(reusableUser.planetaryLifeExpectancy).toBe(36);
+  });
+  test('Should correctly calculate how much longer the user has left to live on Mars', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Mars");
+    expect(reusableUser.planetaryLifeSpan).toEqual(22);
+  });
+  test('Should correctly calculate age on Jupiter', () => {
+    reusableUser.planetCalculator("Jupiter");
+    expect(reusableUser.planetaryAge).toBe(2);
+  });
+  test('Should correctly calculate life expectancy on Jupiter', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Jupiter");
+    expect(reusableUser.planetaryLifeExpectancy).toBe(6);
+  });
+  test('Should correctly calculate how much longer the user has left to live on Jupiter', () => {
+    reusableUser.lifeExpectancyCalculator();
+    reusableUser.planetCalculator("Jupiter");
+    expect(reusableUser.planetaryLifeSpan).toEqual(4);
+  });
+});
+
+describe('User', () => {
+  let reusableUser;
+  beforeEach(() => {
+    reusableUser = new User("Noel", 27, "Female", 10, "less than 1000 miles");
+  });
+
+  test('Should create a new user object with a life expectancy, a Mars age and a Mars life expectancy', () => {
+  });
+  test('Should correctly calculate life expectancy for user', () => {
+    reusableUser.lifeExpectancyCalculator();
+    expect(reusableUser.lifeExpectancy).toBe(76);
   });
   test('Should correctly calculate age on Mercury', () => {
     reusableUser.planetCalculator("Mercury");
